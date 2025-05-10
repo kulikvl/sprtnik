bcdae4a8e04bd00b04019f96002b7166bd7fdb0f
Vladimir Kulikov <vladimir.kulikov@cdn77.com>
2025-05-10 19:43:48 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   build: {
-    outDir: 'dist',
+    target: 'esnext',
+    outDir: '../dist',
     emptyOutDir: true,
   },
 });
